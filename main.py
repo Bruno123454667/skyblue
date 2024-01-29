2bd674e762f5156107b20dd281f2bcbfe825295e
Philip <151831106+PhilipHanHal@users.noreply.github.com>
2024-01-29 11:52:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,85 @@
-# Setup
-yes_no = ["yes", "no"]
-directions = ["left", "right", "forward", "backward"]
+import pandas as pd
+import os
 
-# Introduction
-name = input("What is your name, adventurer?\n")
-print("Greetings, " + name + ". Let us go on a quest!")
-print("You find yourself on the edge of a dark forest.")
-print("Can you find your way through?\n")
+# Hämta vart den ska leta efter filerna 
+dir_path = os.getcwd()
 
-# Start of game
-response = ""
-while response not in yes_no:
-    response = input("Would you like to step into the forest?\nyes/no\n")
-    if response == "yes":
-        print("You head into the forest. You hear crows cawwing in the distance.\n")
-    elif response == "no":
-        print("You are not ready for this quest. Goodbye, " + name + ".")
-        quit()
+# Filnamn för kartan och rum-informationen
+map_file = "Map.xlsx"
+room_information_file = "Enchanted Forest.xlsx"
+
+# Skapa vägarna till Excel-filerna
+map_file_path = os.path.join(dir_path, map_file)
+room_information_path = os.path.join(dir_path, room_information_file)
+
+# Kontrollera om båda filerna finns i mappen
+if os.path.isfile(map_file_path) and os.path.isfile(room_information_path):
+    # Ladda in data från Excel-filerna till dataframes
+    map_data = pd.read_excel(map_file_path)
+    room_information_df = pd.read_excel(room_information_path)
+else:
+    print("Error, en eller båda kalkylarken hittas inte.")
+
+
+def visa_rum(rum_data, aktuellt_rum):
+    # Skriv ut de tillgängliga rummen
+    print("Available rooms:", rum_data['Room'].unique())
+    # Hämta information om det aktuella rummet
+    matchande_rum = rum_data.loc[rum_data['Room'] == aktuellt_rum]
+
+    if not matchande_rum.empty:
+        # Om rummet finns, skriv ut beskrivning och tillgängliga handlingar
+        beskrivning = matchande_rum['Description'].values[0]
+        print(f'Du är i rummet: {aktuellt_rum}')
+        print(f'Beskrivning: {beskrivning}')
+        handlingar = matchande_rum['Actions'].values[0]
+        print(f'Möjliga handlingar: {handlingar}')
+        return beskrivning, handlingar
     else:
-        print("I didn't understand that.\n")
+        # Om rummet inte finns, skriv ut ett felmeddelande
+        print(f"Rummet {aktuellt_rum} finns inte i kalkylbladet.")
+        return None, None
 
-# Next part of game
-response = ""
-while response not in directions:
-    print("To your left, you see a bear.")
-    print("To your right, there is more forest.")
-    print("There is a rock wall directly in front of you.")
-    print("Behind you is the forest exit.\n")
-    response = input("What direction do you move?\nleft/right/forward/backward\n")
-    if response == "left":
-        print("The bear eats you. Farewell, " + name + ".")
-        quit()
-    elif response == "right":
-        print("You head deeper into the forest.\n")
-    elif response == "forward":
-        print("You cannot scale the wall.\n")
-        response = ""
-    elif response == "backward":
-        print("You leave the forest. Goodbye, " + name + ".")
-        quit()
-    else:
-        print("I didn't understand that.\n")
+
+# Hitta nytt Rum
+def hitta_nytt_rum(map_data, aktuellt_rum, riktning):
+    # Hitta det nya rummet baserat på riktningen
+    return map_data.loc[map_data['Room'] == aktuellt_rum, riktning].values[0]
+
+def spela():
+    # Fråga användaren vilket rum de vill börja i
+    aktuellt_rum = input("Vilket rum vill du börja i? ")
+
+    while True:
+        # Kontrollera om det givna rummet finns i rum-informationen
+        if aktuellt_rum not in room_information_df['Room'].values:
+            print(f"Rummet {aktuellt_rum} finns inte i kalkylbladet för rum.")
+            break
+
+        # Visa information om det aktuella rummet
+        beskrivning, handlingar = visa_rum(room_information_df, aktuellt_rum)
+        # Fråga användaren vad de vill göra sen
+        kommando = input("Vad vill du göra? ")
+
+        if kommando.lower() == "quit":
+            break
+
+        # Hämta riktningen från användarens kommando
+        riktning = kommando.lower().split()[-1]
+
+        # Kontrollera om riktningen är giltig
+        if riktning in ['north', 'south', 'east', 'west']:
+            # Hitta det nya rummet baserat på riktningen
+            nytt_rum = hitta_nytt_rum(map_data, aktuellt_rum, riktning)
+        else:
+            print("Ogiltig riktning. Försök igen.")
+            continue
+
+        if not pd.isnull(nytt_rum):
+            # Uppdatera det aktuella rummet till det nya rummet
+            aktuellt_rum = nytt_rum
+        else:
+            print("Ogiltig riktning. Försök igen.")
+
+# Starta spelet
+spela()
